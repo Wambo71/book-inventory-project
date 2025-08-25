7102534f40ecc003f6bce013f015162c1c7ab201
Wambui <wambuikaranja78@gmail.com>
2025-08-25 23:16:51 +0300

    adds data on books,customer and sales

--- crud/crud.py
+++ crud/crud.py
@@ -22,17 +22,37 @@ def add_sale(book_id, customer_id, sale_date, description):
     session.commit()
     print(f"sale added:Book ID {book_id} Customer ID {customer_id} Date {sale_date} Description {description}")  
 
-if __name__ == "__main__":
-  add_book("Django Mastery", "Jane Doe", 450.50, 6)
-  add_customer("John","john@gmail.com", "123 Main St")
-  add_sale(1, 1, date.today(), "John buys Django Mastery")
-
 #fetch data
 def get_all_books():
-    session.query(Book).all() 
+    books = session.query(Book).all() 
+    for book in books:
+        print(f"{book.id}: {book.title} by {book.author} {book.price}")
+
 
 def get_all_customers():
-    session.query(Customer).all() 
+    customers = session.query(Customer).all() 
+    for customer in customers:
+        print(f"{customer.id}: {customer.name}, {customer.email}, {customer.address}")
+
 
 def get_all_sales():
-    session.query(Sale).all()        
+    sales = session.query(Sale).all()
+    for sale in sales:
+        print(f"{sale.id}: {sale.description} on {sale.sale_date}")
+
+
+
+if __name__ == "__main__":
+#   add_book("Django Mastery", "Jane Doe", 450.50, 6)
+    add_book("Software Engineering", "Mary Jane", 340.67, 5)
+    
+    get_all_books()
+
+    #add_customer("John","john@gmail.com", "123 Main St")
+    add_customer("Warui", "warui@gmail.com", "Kitui Street")
+    get_all_customers()
+
+    #add_sale(1, 1, date.today(), "John buys Django Mastery") 
+    add_sale(2, 2, date.today(), "Warui buys a software emgineering book")
+    get_all_sales ()      
+        
