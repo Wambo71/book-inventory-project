e11b6617f93617c9bcd7863024c617de350c354b
Wambui <wambuikaranja78@gmail.com>
2025-08-25 21:19:09 +0300

    creates add book function

--- crud/crud.py
+++ crud/crud.py
@@ -1,3 +1,10 @@
 from models.model import Book, Sale, Customer, Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import session
+
+#Add a book
+def add_book(title,author,price,stock):
+    new_book = Book(title=title,author=author,stock=stock)
+    session.add(new_book)
+    session.commit()
+    print(f"book '{title}'was added successfully")
