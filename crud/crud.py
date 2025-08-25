27f971e8c576f9f17a832073e2352c40b467de4e
Wambui <wambuikaranja78@gmail.com>
2025-08-25 22:26:39 +0300

    creates fetch data functions

--- crud/crud.py
+++ crud/crud.py
@@ -20,4 +20,19 @@ def add_sale(book_id, customer_id, sale_date, description):
     new_sale = Sale(book_id=book_id, customer_id=customer_id, sale_date=sale_date,description=description)
     session.add(new_sale)
     session.commit()
-    print(f"sale added:Book ID {book_id} Customer ID {customer_id} Date {sale_date} Description {description}")     
+    print(f"sale added:Book ID {book_id} Customer ID {customer_id} Date {sale_date} Description {description}")  
+
+if __name__ == "__main__":
+  add_book("Django Mastery", "Jane Doe", 450.50, 6)
+  add_customer("John","john@gmail.com", "123 Main St")
+  add_sale(1, 1, date.today(), "John buys Django Mastery")
+
+#fetch data
+def get_all_books():
+    session.query(Book).all() 
+
+def get_all_customers():
+    session.query(Customer).all() 
+
+def get_all_sales():
+    session.query(Sale).all()        
