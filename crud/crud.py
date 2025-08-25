531285ee9d046839292554331b40e556f89cb7c4
Wambui <wambuikaranja78@gmail.com>
2025-08-25 21:20:21 +0300

    updates price in add book function

--- crud/crud.py
+++ crud/crud.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import session
 
 #Add a book
 def add_book(title,author,price,stock):
-    new_book = Book(title=title,author=author,stock=stock)
+    new_book = Book(title=title,author=author,price=price,stock=stock)
     session.add(new_book)
     session.commit()
     print(f"book '{title}'was added successfully")
