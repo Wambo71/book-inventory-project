161b3c9edca6c22e5f4547102496dc51479f982c
Wambui <wambuikaranja78@gmail.com>
2025-08-25 21:50:36 +0300

    creates add sale function

--- crud/crud.py
+++ crud/crud.py
@@ -1,6 +1,5 @@
-from models.model import Book, Sale, Customer, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import session
+from models.model import Book, Sale, Customer, session
+from datetime import date
 
 #Add a book
 def add_book(title,author,price,stock):
@@ -14,4 +13,11 @@ def add_customer(name,email,address):
     new_customer= Customer(name=name, email=email, address=address) 
     session.add(new_customer) 
     session.commit()
-    print(f"customer '{name} {email} {address}'was added succesfully") 
+    print(f"customer '{name} {email} {address}'was added succesfully")
+
+#add sale
+def add_sale(book_id, customer_id, sale_date, description):
+    new_sale = Sale(book_id=book_id, customer_id=customer_id, sale_date=sale_date,description=description)
+    session.add(new_sale)
+    session.commit()
+    print(f"sale added:Book ID {book_id} Customer ID {customer_id} Date {sale_date} Description {description}")     
