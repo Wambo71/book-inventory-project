bd7799578def79da798ea3c2d8e15e6350791864
Wambui <wambuikaranja78@gmail.com>
2025-08-25 21:27:04 +0300

    creates add customer function

--- crud/crud.py
+++ crud/crud.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import session
 
 #Add a book
 def add_book(title,author,price,stock):
-    new_book = Book(title=title,author=author,price=price,stock=stock)
+    new_book = Book(title=title, author=author, price=price, stock=stock)
     session.add(new_book)
     session.commit()
     print(f"book '{title}'was added successfully")
+
+ #add customer
+def add_customer(name,email,address):
+    new_customer= Customer(name=name, email=email, address=address) 
+    session.add(new_customer) 
+    session.commit()
+    print(f"customer '{name} {email} {address}'was added succesfully") 
